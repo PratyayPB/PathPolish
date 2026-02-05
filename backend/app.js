db3a3174211ec036f856fd087ce4c3b38c07c10b
Pratyay <typrotypro220@gmail.com>
2026-02-05 20:56:50 +0530

    Auth bug fix for all devices

--- backend/app.js
+++ backend/app.js
@@ -65,6 +65,7 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({
       mongoUrl: process.env.MONGODB_URI,
+      collectionName: "sessions",
       ttl: 24 * 60 * 60, // 1 day
     }),
     cookie: {
